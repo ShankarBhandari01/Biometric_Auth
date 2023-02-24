078977f3a44f3ed71c2852297915a5aa4f2b8b51
shankar <harryshankar@outlook.com>
2023-02-24 12:27:35 +0545

    added encryption

--- app/src/main/java/com/shankar/biometricauth/MainActivity.kt
+++ app/src/main/java/com/shankar/biometricauth/MainActivity.kt
@@ -2,23 +2,45 @@ package com.shankar.biometricauth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
+import android.util.Log
 import android.widget.Toast
 import androidx.biometric.BiometricPrompt
 import androidx.core.content.ContextCompat
 import com.shankar.biometricauth.databinding.ActivityMainBinding
+import java.nio.charset.Charset
+import java.security.KeyStore
+import java.util.*
 import java.util.concurrent.Executor
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private val KEY_NAME ="biometric"
     private lateinit var executor: Executor
     private lateinit var biometricPrompt: BiometricPrompt
     private lateinit var promptInfo: BiometricPrompt.PromptInfo
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        initBiometeric()
+        initBiometric()
+        generateSecretKey(KeyGenParameterSpec.Builder(
+            KEY_NAME,
+            KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
+            .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+            .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+            .setUserAuthenticationRequired(true)
+            // Invalidate the keys if the user has registered a new biometric
+            // credential, such as a new fingerprint. Can call this method only
+            // on Android 7.0 (API level 24) or higher. The variable
+            // "invalidatedByBiometricEnrollment" is true by default.
+            .setInvalidatedByBiometricEnrollment(true)
+            .build())
         binding.btnOpen.setOnClickListener {
             promptAuthenticate()
         }
@@ -26,10 +48,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun promptAuthenticate() {
-        biometricPrompt.authenticate(promptInfo)
+
+        val cipher = getCipher()
+        val secretKey = getSecretKey()
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey)
+        biometricPrompt.authenticate(promptInfo, BiometricPrompt.CryptoObject(cipher))
     }
 
-    private fun initBiometeric() {
+    private fun initBiometric() {
         executor = ContextCompat.getMainExecutor(this)
         biometricPrompt = BiometricPrompt(this, executor,
             object : BiometricPrompt.AuthenticationCallback() {
@@ -47,8 +73,16 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(
                         applicationContext,
                         "Authentication succeeded!", Toast.LENGTH_SHORT
+                    ).show()
+                    val encryptedInfo: ByteArray? = result.cryptoObject?.cipher?.doFinal(
+                        // plaintext-string text is whatever data the developer would like
+                        // to encrypt. It happens to be plain-text in this example, but it
+                        // can be anything
+                        "mypin".toByteArray(Charset.defaultCharset())
                     )
-                        .show()
+                    Log.d("MY_APP_TAG", "Encrypted information: " +
+                            Arrays.toString(encryptedInfo))
+
                 }
 
                 override fun onAuthenticationFailed() {
@@ -66,4 +100,26 @@ class MainActivity : AppCompatActivity() {
             .setNegativeButtonText("Use account password")
             .build()
     }
+
+    private fun generateSecretKey(keyGenParameterSpec: KeyGenParameterSpec) {
+        val keyGenerator = KeyGenerator.getInstance(
+            KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore")
+        keyGenerator.init(keyGenParameterSpec)
+        keyGenerator.generateKey()
+    }
+    private fun getSecretKey(): SecretKey {
+        val keyStore = KeyStore.getInstance("AndroidKeyStore")
+
+        // Before the keystore can be accessed, it must be loaded.
+        keyStore.load(null)
+        return keyStore.getKey(KEY_NAME, null) as SecretKey
+    }
+    private fun getCipher(): Cipher {
+        return Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/"
+                + KeyProperties.BLOCK_MODE_CBC + "/"
+                + KeyProperties.ENCRYPTION_PADDING_PKCS7)
+    }
+
+
+
 }
